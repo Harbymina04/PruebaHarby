f6854eea445f2e1e3859cc86224ca6a15b2f88c7
Harbymina04 <Harbymina8@gmail.com>
2020-11-27 10:15:40 -0500

    mi primer commit

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("Sap.Tutorial.PruebaHarby.controller.Main", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"Sap/Tutorial/PruebaHarby/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
